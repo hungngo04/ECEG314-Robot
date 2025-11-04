cf33a8bab4e617f27ff32ef4efc477f3a4884c90
Hung Ngo <ngotienhung195@gmail.com>
2025-11-04 08:25:17 -0500

    drive class

--- drivetest.py
+++ drivetest.py
@@ -1,95 +1,54 @@
 import sys
 import time
 import math
-from machine import Pin, ADC
+import machine
 
-M1A = machine.PWM(machine.Pin(8))
-M1B = machine.PWM(machine.Pin(9))
-M2A = machine.PWM(machine.Pin(10))
-M2B = machine.PWM(machine.Pin(11))
-M1A.freq(8_000)
-M1B.freq(8_000)
-M2A.freq(8_000)
-M2B.freq(8_000)
-def drive(speed, omega):
-    omega = omega*-1
-    v_right = ((2*speed)+(omega*13/2))/2
-    v_left = 2*speed-v_right
-    print(v_left, v_right)
-    #convert to duty cycle from here
+class RobotDrive():
+    def __init__(self):
+        self.M1A = machine.PWM(machine.Pin(8))
+        self.M1B = machine.PWM(machine.Pin(9))
+        self.M2A = machine.PWM(machine.Pin(10))
+        self.M2B = machine.PWM(machine.Pin(11))
 
-    if (speed ==0 and omega==0):
-        M1A.duty_u16(0)
-        M1B.duty_u16(0)
-        M2A.duty_u16(0)
-        M2B.duty_u16(0)
-    elif (speed == 0 and omega != 0):
-        #1/2 * pi*13
-        #vl = -vr
-        if v_left >= 0:
-            M1A.duty_u16(int(1176*abs(v_left) + 1274))
-            M1B.duty_u16(0)
-            M2A.duty_u16(0)
-            M2B.duty_u16(-1*int(1176*abs(v_left) + 1274))
-        else:
-            M1A.duty_u16(0)
-            M1B.duty_u16(int(1176*abs(v_right) + 1274))
-            M2A.duty_u16(-1*int(1176*abs(v_right) + 1274))
-            M2B.duty_u16(0)
-        
-        
-    else:
-        if v_left >= 0:
-            M1A.duty_u16(int(1176*abs(v_left) + 1274))
-            M1B.duty_u16(0)
-        else:
-            M1B.duty_u16(int(1176*abs(v_left) + 1274))
-            M1A.duty_u16(0)
-        if v_right >= 0:
-            M2A.duty_u16(int(1176*abs(v_right) + 1274))
-            M2B.duty_u16(0)
-        else:
-            M2B.duty_u16(int(1176*abs(v_right) + 1274))
-            M2A.duty_u16(0)
-
+        self.M1A.freq(8_000)
+        self.M1B.freq(8_000)
+        self.M2A.freq(8_000)
+        self.M2B.freq(8_000)
     
+    def drive(self, speed, omega):
+        omega = omega*-1
+        v_right = ((2*speed)+(omega*13/2))/2
+        v_left = 2*speed-v_right
 
+        duty_cycle_left = int(1176*abs(v_left) + 1274)
+        duty_cycle_right = int(1176*abs(v_right) + 1274)
 
-# TEST 1 -- straight!
-# 20 cm/s, 0 rad/s 
-time.sleep_ms(1000)
-drive (20, 0)
-time.sleep_ms(500)
-# stop, robot should have gone 10 cm forward. Check! I got about 12 cm.
-drive (0, 0)
-time.sleep_ms(500)
-
-
-# TEST 2 -- reverse
-# -20 cm/s, 0 rad/s 
-drive (-20, 0)
-time.sleep_ms(500)
-# stop, robot should have gone 10 cm in reverse. Drive the M1B and M2B pins!
-drive (0, 0)
-time.sleep_ms(500)
-
-# TEST 3 -- RIGHT
-drive (20, math.radians(180))
-time.sleep_ms(500)
-# stop, robot should have turned about 90 degrees clockwise
-drive (0, 0)
-time.sleep_ms(500)
-
-# TEST 4 -- LEFT
-drive (20, -math.radians(180))
-time.sleep_ms(500)
-# stop, we should have turned about 180 degrees counter clockwise
-drive (0, 0)
-time.sleep_ms(500)
-
-# TEST 5 -- ROTATE in PLACE
-drive (0, math.radians(360))
-time.sleep_ms(1000)
-# stop, robot should turn about 360 degrees clockwise
-drive (0, 0)
-time.sleep_ms(500)
+        if (speed == 0 and omega == 0):
+            self.M1A.duty_u16(0)
+            self.M1B.duty_u16(0)
+            self.M2A.duty_u16(0)
+            self.M2B.duty_u16(0)
+        elif (speed == 0 and omega != 0):
+            if v_left >= 0:
+                self.M1A.duty_u16(duty_cycle_left)
+                self.M1B.duty_u16(0)
+                self.M2A.duty_u16(0)
+                self.M2B.duty_u16(-duty_cycle_left)
+            else:
+                self.M1A.duty_u16(0)
+                self.M1B.duty_u16(duty_cycle_right)
+                self.M2A.duty_u16(-duty_cycle_right)
+                self.M2B.duty_u16(0)
+        else:
+            if v_left >= 0:
+                self.M1A.duty_u16(duty_cycle_left)
+                self.M1B.duty_u16(0)
+            else:
+                self.M1B.duty_u16(duty_cycle_left)
+                self.M1A.duty_u16(0)
+            if v_right >= 0:
+                self.M2A.duty_u16(duty_cycle_right)
+                self.M2B.duty_u16(0)
+            else:
+                self.M2B.duty_u16(duty_cycle_right)
+                self.M2A.duty_u16(0)
--- test_drive.py
+++ test_drive.py
@@ -1,35 +1,44 @@
 import time
 import math
-from drive import RobotDrive
+from drivetest import RobotDrive
 
-robot = RobotDrive(bias=-0.05, speed_to_duty_factor=1192, wheelbase=13.0)
+robot = RobotDrive()
 
-# TEST 1 - straight!
+# TEST 1 -- straight!
+# 20 cm/s, 0 rad/s 
+time.sleep_ms(1000)
 robot.drive(20, 0)
 time.sleep_ms(500)
-robot.stop()
+# stop, robot should have gone 10 cm forward. Check! I got about 12 cm.
+robot.drive (0, 0)
 time.sleep_ms(500)
 
-# TEST 2 - reverse
-robot.drive(-20, 0)
+
+# TEST 2 -- reverse
+# -20 cm/s, 0 rad/s 
+robot.drive (-20, 0)
 time.sleep_ms(500)
-robot.stop()
+# stop, robot should have gone 10 cm in reverse. Drive the M1B and M2B pins!
+robot.drive (0, 0)
 time.sleep_ms(500)
 
-# TEST 3 - right
-robot.drive(20, math.radians(180))
+# TEST 3 -- RIGHT
+robot.drive (20, math.radians(180))
 time.sleep_ms(500)
-robot.stop()
-time.sleep_ms(500)
-
-# TEST 4 - left
-robot.drive(20, -math.radians(180))
-time.sleep_ms(500)
-robot.stop()
+# stop, robot should have turned about 90 degrees clockwise
+robot.drive (0, 0)
 time.sleep_ms(500)
 
-# TEST 5 - rotate in place
-robot.drive(0, math.radians(360))
+# TEST 4 -- LEFT
+robot.drive (20, -math.radians(180))
+time.sleep_ms(500)
+# stop, we should have turned about 180 degrees counter clockwise
+robot.drive (0, 0)
+time.sleep_ms(500)
+
+# TEST 5 -- ROTATE in PLACE
+robot.drive (0, math.radians(360))
 time.sleep_ms(1000)
-robot.stop()
+# stop, robot should turn about 360 degrees clockwise
+robot.drive (0, 0)
 time.sleep_ms(500)
